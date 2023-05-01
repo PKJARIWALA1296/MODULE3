b6b15f0ced0d7efd9428dad5cef3ea08469c1dd2
JARIWALA PRATIK KISHORBHAI <130529667+PKJARIWALA1296@users.noreply.github.com>
2023-05-01 08:33:28 +0530

    Delete Que1.c

--- Que1.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-main(){
-	printf(" My Name is Pratik Jariwala.");
-	printf("\n My Date of Birth is 12th January 1996.");
-	printf("\n I Am 27yr Old.");
-	printf("\n My Address is Palanpur Patiya Rander Road, Surat.");
-}
