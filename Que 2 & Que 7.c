021390d15ffe5d2e864bc74a3b2d3edc0c452f31
JARIWALA PRATIK KISHORBHAI <130529667+PKJARIWALA1296@users.noreply.github.com>
2023-05-12 09:16:51 +0530

    Add files via upload

---
+++ Que 2 & Que 7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+main(){
+	int A,B,add,sub,multi,modu;
+	float div;
+	printf("\n Enter the Value Of A & B :");
+	scanf("%d %d",&A,&B);
+	add=A+B;
+	printf("\n Addition= %d",add);
+	sub=A-B;
+	printf("\n Subtraction= %d",sub);
+	multi=A*B;
+	printf("\n Multiplication= %d",multi);
+	div=(float)A/B;
+	printf("\n Division= %.2f",div);
+	modu=A%B;
+	printf("\n Modulation= %d",modu);
+}
