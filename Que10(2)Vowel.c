3b7c81560e80c87f182f139911c7a5592fee4428
JARIWALA PRATIK KISHORBHAI <130529667+PKJARIWALA1296@users.noreply.github.com>
2023-05-22 09:31:51 +0530

    Add files via upload

---
+++ Que10(2)Vowel.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+main (){
+	char ch;
+	printf("\n Enter the Character :");
+	scanf("%c",&ch);
+	switch(ch){
+		case 'a':
+			//if(ch=='a'){
+			  printf("\n Given Character is Vowel.");
+			//}
+		break;
+		case 'A':
+			//if(ch=='A'){
+			  printf("\n Given Character is Vowel.");
+			//}
+		break;
+		case 'e':
+			//if(ch=='e'){
+			  printf("\n Given Character is Vowel.");
+			//}
+		break;
+		case 'E':
+			//if(ch=='E'){
+			  printf("\n Given Character is Vowel.");
+			//}
+		break;
+		case 'i':
+			//if(ch=='i'){
+			  printf("\n Given Character is Vowel.");
+			//}
+		break;
+		case 'I':
+			//if(ch=='I'){
+			  printf("\n Given Character is Vowel.");
+			//}
+		break;
+		case 'o':
+			//if(ch=='o'){
+			  printf("\n Given Character is Vowel.");
+			//}
+		break;
+		case'O':
+			//if(ch=='O'){
+			  printf("\n Given Character is Vowel.");
+			//}
+		break;
+		case 'u':
+			//if(ch=='u'){
+			  printf("\n Given Character is Vowel.");
+			//}
+		break;
+		case 'U':
+			//if(ch=='U'){
+			  printf("\n Given Character is Vowel.");
+			//}
+		break;
+		default:
+			printf("\n Given Character is Consonants.");
+		break;
+}
+}
